7e970aaa259dc9cf8bf670e65b7d41d12aa3fbb3
russell wang <russell.wang@mail.utoronto.ca>
2021-04-12 16:44:54 -0700

    baseline with random noise added

--- baseline/encoder/params_model.py
+++ baseline/encoder/params_model.py
@@ -4,9 +4,9 @@ num_class = 81313
 
 ## Training parameters
 learning_rate_init = 0.01
-cls_per_batch = 48
-img_per_cls = 5
+cls_per_batch = 32
+img_per_cls = 8
 
-v_cls_per_batch = 48
-v_img_per_cls = 5
+v_cls_per_batch = 32
+v_img_per_cls = 8
 num_validate = 8
